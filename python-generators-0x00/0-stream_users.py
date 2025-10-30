7a0bc797aebb78688491387465e3097964bb1009
Daniel Komolafe <daniel@komolafe.dev>
2025-10-29 21:45:46 -0600

    .

---
+++ python-generators-0x00/0-stream_users.py
@@ -0,0 +1,16 @@
+seed = __import__("seed")
+
+
+def stream_users():
+    connection = seed.connect_to_prodev()
+    if connection is None:
+        return None
+
+    cur = connection.cursor(buffered=True)
+
+    cur.execute("SELECT * FROM user_data;")
+    while True:
+        row = cur.fetchone()
+        if row is None:
+            break
+        yield row
