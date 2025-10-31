cbd18aae4f847463d4333cfa60ea900277521853
Daniel Komolafe <daniel@komolafe.dev>
2025-10-30 20:29:06 -0600

    .

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -31,4 +31,5 @@ def batch_processing(batch_size: int):
     for batch in stream_users_in_batches(batch_size=batch_size):
         for user in batch:
             if int(user["age"]) > 25:
-                print(user)
+                return user
+    return
