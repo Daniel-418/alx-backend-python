585949cc1b740a52b0294384a5ad1dc8ed120f9a
Daniel Komolafe <daniel@komolafe.dev>
2025-11-23 13:19:20 -0600

    finished task

--- messaging_app/chats/pagination.py
+++ messaging_app/chats/pagination.py
@@ -1,6 +1,17 @@
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 
 
 class MessagesPagination(PageNumberPagination):
     page_size = 20
     ordering = "-sent_at"
+
+    def get_paginated_response(self, data):
+        return Response(
+            {
+                "count": self.page.paginator.count,
+                "next": self.get_next_link(),
+                "previous": self.get_previous_link,
+                "results": data,
+            }
+        )
